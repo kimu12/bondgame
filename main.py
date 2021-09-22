49e0764478570b60bb2a99fd8ef406ab66ed3b4e
georgebutler <georgebutleriv@gmail.com>
2021-09-21 20:23:09 -0700

    I added the current time to print()

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print('Hi')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Hi from 20:22')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
