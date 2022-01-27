5c8bfac92768547ecfba8dc96f75293d5e9a9745
Roman Piñango <alaguairavoy@gmail.com>
2022-01-27 13:54:09 -0500

    - Changing pieces into own groups - Creating list of groups - Better indentation

--- main.py
+++ main.py
@@ -25,38 +25,56 @@ RED = ( 255, 0, 0)
 generate_starting_pos() #generates a semi-random list of starting positions for sprites so they don't overlap
 
 #This will be a list that will contain all the sprites for our game.
-ion_sprites_group = pygame.sprite.Group()
-ion_sprites_list = []
+# ion_sprites_group = pygame.sprite.Group()
 
-ion_plus_1 = Ion_sprite(1, screen_width, screen_height, screen)
-ion_sprites_group.add(ion_plus_1)
-ion_sprites_group.add(ion_plus_1.group)
+ion_sprites_groups = []
 
-ion_plus_2 = Ion_sprite(2, screen_width, screen_height, screen)
-ion_sprites_group.add(ion_plus_2)
-ion_sprites_group.add(ion_plus_2.group)
+ion_group_1 = Ion_group()
+ion_plus = Ion_sprite(1, screen_width, screen_height, screen)
+ion_group_1.add(ion_plus)
+ion_group_1.add(ion_plus.group)
 
-ion_plus_3 = Ion_sprite(3, screen_width, screen_height, screen)
-ion_sprites_group.add(ion_plus_3)
-ion_sprites_group.add(ion_plus_3.group)
+ion_sprites_groups.append(ion_group_1)
 
-ion_neg_1 = Ion_sprite(-1, screen_width, screen_height, screen)
-ion_sprites_group.add(ion_neg_1)
-ion_sprites_group.add(ion_neg_1.group)
+ion_group_2 = Ion_group()
+ion_plus = Ion_sprite(2, screen_width, screen_height, screen)
+ion_group_2.add(ion_plus)
+ion_group_2.add(ion_plus.group)
 
-ion_neg_2 = Ion_sprite(-2, screen_width, screen_height, screen)
-ion_sprites_group.add(ion_neg_2)
-ion_sprites_group.add(ion_neg_2.group)
+ion_sprites_groups.append(ion_group_2)
+
+ion_group_3 = Ion_group()
+ion_plus = Ion_sprite(3, screen_width, screen_height, screen)
+ion_group_3.add(ion_plus)
+ion_group_3.add(ion_plus.group)
+
+ion_sprites_groups.append(ion_group_3)
+
+ion_group_4 = Ion_group()
+ion_neg = Ion_sprite(-1, screen_width, screen_height, screen)
+ion_group_4.add(ion_neg)
+ion_group_4.add(ion_neg.group)
+
+ion_sprites_groups.append(ion_group_4)
+
+ion_group_5 = Ion_group()
+ion_neg = Ion_sprite(-2, screen_width, screen_height, screen)
+ion_group_5.add(ion_neg)
+ion_group_5.add(ion_neg.group)
+
+ion_sprites_groups.append(ion_group_5)
 
 #FIXME:  there's an int getting added to ion_sprites_group here somehow
-ion_neg_3 = Ion_sprite(-3, screen_width, screen_height, screen)
-ion_sprites_group.add(ion_neg_3)
-ion_sprites_group.add(ion_neg_3.group)
+ion_group_6 = Ion_group()
+ion_neg = Ion_sprite(-3, screen_width, screen_height, screen)
+ion_group_6.add(ion_neg)
+ion_group_6.add(ion_neg.group)
 
-for i, object in enumerate(ion_sprites_group):
-    #if type(object) == int:
-    print('{}: {}, type: {}'.format(i+1, object, type(object)))
+ion_sprites_groups.append(ion_group_6)
 
+# for i, object in enumerate(ion_sprites_group):
+    # if type(object) == int:
+    # print('{}: {}, type: {}'.format(i+1, object, type(object)))
 
 selected = None
 
@@ -65,59 +83,61 @@ clock = pygame.time.Clock()
 
 # -------- Main Program Loop -----------
 while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-        #FIXME: drag & drop code:
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1: #'1' - Is left mousebutton being clicked?
-                for object in ion_sprites_group:
-                    print(type(object))
-                #print (ion_sprites_group) --> this yields "<Group(24 sprites)>"
-                #print (type(ion_sprites_group)) --> this yields "<class 'pygame.sprite.Group'>"
-                for i, puzzle_piece in enumerate(ion_sprites_group):
-                    #if type(puzzle_piece) != int: #Delete this 'if' statement line once the int is nolonger sneaking into ion_sprites_group
-                    #FIXME: AttributeError: 'list' object has no attribute 'collidepoint': puzzle_piece.rect[1].collidepoint(event.pos):
-                    #so this object is a list... when "puzzle_piece.rect[0].collidepoint(event.pos):", assuming 1st item in list is rect object...
-                    #program closes 'Process finished with exit code 1' when clicking on sprite...
-                    if puzzle_piece.rect.collidepoint(event.pos):
-                        selected = i
-                        selected_offset_x = puzzle_piece.x - event.pos[0]
-                        selected_offset_y = puzzle_piece.y - event.pos[1]
+    #FIXME: drag & drop code:
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1: #'1' - Is left mousebutton being clicked?
+            # for object in ion_sprites_group:
+            #    print(type(object))
+            #print (ion_sprites_group) --> this yields "<Group(24 sprites)>"
+            #print (type(ion_sprites_group)) --> this yields "<class 'pygame.sprite.Group'>"
+            #for i, puzzle_piece in enumerate(ion_sprites_group):
+            for puzzle_piece in ion_sprites_groups:
+                #if type(puzzle_piece) != int: #Delete this 'if' statement line once the int is nolonger sneaking into ion_sprites_group
+                #FIXME: AttributeError: 'list' object has no attribute 'collidepoint': puzzle_piece.rect[1].collidepoint(event.pos):
+                #so this object is a list... when "puzzle_piece.rect[0].collidepoint(event.pos):", assuming 1st item in list is rect object...
+                #program closes 'Process finished with exit code 1' when clicking on sprite...
+                if puzzle_piece.rect.collidepoint(event.pos):
+                    # selected = i
+                    selected_offset_x = puzzle_piece.x - event.pos[0]
+                    selected_offset_y = puzzle_piece.y - event.pos[1]
 
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                selected = None
+    elif event.type == pygame.MOUSEBUTTONUP:
+        if event.button == 1:
+            selected = None
 
-        elif event.type == pygame.MOUSEMOTION:
-            if selected is not None:  # selected can be `0` so `is not None` is required
-                # move object
-                #FIXME: Class 'Group' does not define '__getitem__', so the [] operator cannot be used on its instances
-                ion_sprites_group[selected].x = event.pos[0] + selected_offset_x
-                ion_sprites_group[selected].y = event.pos[1] + selected_offset_y
+    elif event.type == pygame.MOUSEMOTION:
+        if selected is not None:  # selected can be `0` so `is not None` is required
+            # move object
+            #FIXME: Class 'Group' does not define '__getitem__', so the [] operator cannot be used on its instances
+            ion_sprites_group[selected].x = event.pos[0] + selected_offset_x
+            ion_sprites_group[selected].y = event.pos[1] + selected_offset_y
 
 
-        pygame.display.flip()
+    pygame.display.flip()
 
     # --- Game logic should go here
-        ion_sprites_group.update()
 
     # --- Drawing code should go here
-        screen.fill(background_green)
+    screen.fill(background_green)
 
     # Draw sprites
-        ion_sprites_group.draw(screen)
+    for group in ion_sprites_groups:
+        group.update()
+        group.draw(screen)
 
     # Write text
-        pygame.display.flip()
+    pygame.display.flip()
 
 
 
 
     # --- Limit to 60 frames per second
-        clock.tick(60)
+    clock.tick(60)
 
 # Once we have exited the main program loop we can stop the game engine:
 pygame.quit()
